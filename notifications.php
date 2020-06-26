9b3e9a5c25fc15a630a6e6e55d8ddb65b903d38a
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 11:55:53 -0500

    http response

--- notifications.php
+++ notifications.php
@@ -2,6 +2,8 @@
 
 MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
 
+http_response_code(200);
+
 switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
