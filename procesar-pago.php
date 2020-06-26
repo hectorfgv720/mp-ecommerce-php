5db8c9d43e68b27dbdc1bea88f3bfb14ce15f032
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 13:14:52 -0500

    minor changes

--- procesar-pago.php
+++ procesar-pago.php
@@ -26,15 +26,18 @@
 		"area_code" => "52",
 		"number" => "5549737300"
 	);
+
+	$payer->identification = array(
+        "number"  =>  "22333444",
+        "type" => "DNI"
+    );
+
 	$payer->address = array(
 		"street_name" => "Insurgentes Sur",
 		"street_number" => 1602,
 		"zip_code" => "03940"
-    );
-    $payer->identification = array(
-        "number"  =>  "22333444",
-        "type" => "DNI"
-    );
+	);	
+	
     $preference->payer = $payer;
 
 	$preference->payment_methods = array(
