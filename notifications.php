f7ad590485ad3e7966a8aa6e7a4a40d5144c30be
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 16:33:12 -0500

    13

--- notifications.php
+++ notifications.php
@@ -3,9 +3,9 @@
 if($json = json_decode(file_get_contents("php://input"), true)) {
     print_r($json);
     $data = $json;
+    echo("si jala");
 }else {
     print_r($_POST);
-    $data = $_POST;
     echo("no jalo");
     }
 
