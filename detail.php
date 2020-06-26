dae85485ec434c8699779ed00720896020a6883a
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 10:41:23 -0500

    MLM

--- detail.php
+++ detail.php
@@ -133,7 +133,7 @@
                                     </div>
                                     <form action="pago.php" method="POST">
                                         <script
-                                            src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                                            src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
                                             data-preference-id="<?php echo $preference->id; ?>"
                                             data-button-label="Pagar la compra"
                                             data-header-color="#81ecec"
