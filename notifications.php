06af7aa70223bdb954c064d7692758a47ec5af98
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 12:10:41 -0500

    some minor changes

--- notifications.php
+++ notifications.php
@@ -14,19 +14,19 @@ http_response_code(200);
 
 switch($_POST["type"]) {
     case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        $response = MercadoPago\Payment.find_by_id($_POST["id"]);
         break;
     case "plan":
-        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        $response = MercadoPago\Plan.find_by_id($_POST["id"]);
         break;
     case "subscription":
-        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        $response = MercadoPago\Subscription.find_by_id($_POST["id"]);
         break;
     case "invoice":
-        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        $response = MercadoPago\Invoice.find_by_id($_POST["id"]);
         break;
 }
 
-print_r($payment);
+json_encode($response);
 
 ?>
