313109f8bdb18a8f1932098a662803941f39c5fc
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 12:03:02 -0500

    post allow

--- notifications.php
+++ notifications.php
@@ -1,5 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
 
 http_response_code(200);
