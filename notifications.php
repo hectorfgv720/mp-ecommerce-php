f8e0af9d781e7c2209f7e83c1542fa20482d99d8
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 13:40:59 -0500

    test

--- notifications.php
+++ notifications.php
@@ -1,36 +1,36 @@
-<?php
+<?php 
+$post_data = $_POST['data'];
 
-require __DIR__ . '/vendor/autoload.php';
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
-
-http_response_code(200);
-
-switch($_POST["type"]) {
-    case "payment":
-        $response = MercadoPago\Payment.find_by_id($_POST["id"]);
-        break;
-    case "plan":
-        $response = MercadoPago\Plan.find_by_id($_POST["id"]);
-        break;
-    case "subscription":
-        $response = MercadoPago\Subscription.find_by_id($_POST["id"]);
-        break;
-    case "invoice":
-        $response = MercadoPago\Invoice.find_by_id($_POST["id"]);
-        break;
+if (!empty($post_data)) {
+    $filename = 'log.json';
+    $handle = fopen($filename, "w");
+    fwrite($handle, $post_data);
+    fclose($handle);
+    echo $file;
 }
 
-json_encode($response);
+require_once './vendor/autoload.php';
 
-var_dump(http_response_code(201));
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+MercadoPago\SDK::setPublicKey('APP_USR-158fff95-0bdf-4149-9abc-c8b0ac7f289f');
+MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948"); 
 
-var_dump(http_response_code());
+?>
+<?php
+    switch($_POST["type"]) {
+        case "payment":
+            $data = MercadoPago\Payment::find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $data = MercadoPago\Plan::find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $data = MercadoPago\Subscription::find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $data = MercadoPago\Invoice::find_by_id($_POST["id"]);
+            break;
+    }
 
+    print_r($data);
 ?>
