62697f092c3351171462e214b5a7f0815b406cbf
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 14:57:34 -0500

    asdf

--- notifications.php
+++ notifications.php
@@ -1,7 +1,36 @@
 <?php 
-$request = file_get_contents('php://input');
-$input = json_decode($request);
+$post_data = $_POST['data'];
 
-file_put_contents('output.txt', var_export($input, TRUE)); 
-file_put_contents('output.txt', $request);
+if (!empty($post_data)) {
+    $filename = 'log.json';
+    $handle = fopen($filename, "w");
+    fwrite($handle, $post_data);
+    fclose($handle);
+    echo $file;
+}
+
+require_once './vendor/autoload.php';
+
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+MercadoPago\SDK::setPublicKey('APP_USR-158fff95-0bdf-4149-9abc-c8b0ac7f289f');
+MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948"); 
+
+?>
+<?php
+    switch($_POST["type"]) {
+        case "payment":
+            $data = MercadoPago\Payment::find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $data = MercadoPago\Plan::find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $data = MercadoPago\Subscription::find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $data = MercadoPago\Invoice::find_by_id($_POST["id"]);
+            break;
+    }
+
+    print_r($data);
 ?>
