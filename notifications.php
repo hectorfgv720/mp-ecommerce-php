cbaf39c8645c670e451cd84edbe1241f0693ace3
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 11:22:06 -0500

    http change

--- notifications.php
+++ notifications.php
@@ -2,18 +2,18 @@
 
     MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
 
-    switch($_GET["type"]) {
+    switch($_POST["type"]) {
         case "payment":
-            $response = MercadoPago\Payment.find_by_id($_GET["id"]);
+            $response = MercadoPago\Payment.find_by_id($_POST["id"]);
             break;
         case "plan":
-            $response = MercadoPago\Plan.find_by_id($_GET["id"]);
+            $response = MercadoPago\Plan.find_by_id($_POST["id"]);
             break;
         case "subscription":
-            $response = MercadoPago\Subscription.find_by_id($_GET["id"]);
+            $response = MercadoPago\Subscription.find_by_id($_POST["id"]);
             break;
         case "invoice":
-            $response = MercadoPago\Invoice.find_by_id($_GET["id"]);
+            $response = MercadoPago\Invoice.find_by_id($_POST["id"]);
             break;
     }
 
