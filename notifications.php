497f463458e05d293be0ad22027334586cb0222f
Hector Garcia <hectorfgv@gmail.com>
2020-06-26 11:37:38 -0500

    webhook test

--- notifications.php
+++ notifications.php
@@ -1,22 +1,20 @@
 <?php
 
-    MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
+MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
 
-    switch($_POST["type"]) {
-        case "payment":
-            $response = MercadoPago\Payment.find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $response = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $response = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $response = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-    }
-
-    print_r($response);
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        break;
+}
 
 ?>
