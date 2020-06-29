ef33823d83ed11a432705bba89e347370fa76ab1
Hector Garcia <hectorfgv@gmail.com>
2020-06-29 11:37:53 -0500

    1

--- notifications.php
+++ notifications.php
@@ -1,19 +1,13 @@
 <?php 
     header('Content-Type: application/json');
+    header("refresh: 3;");
 
     $json = json_decode(file_get_contents('php://input'), true);
     $json_encode = json_encode($json);
 
     $_COOKIE["json"] = $json_encode;
 
-    if (isset($_COOKIE["json"])){
-        echo "
-            <script>
-                window.location.reload(true);
-            </script>
-        ";
-        echo $json_encode;
-    }else{
-        echo "no se ha definido la cookie";
+    if (isset($_COOKIE["json"]) && $_COOKIE != ""){
+        echo $_COOKIE["json"];
     }
 ?>
