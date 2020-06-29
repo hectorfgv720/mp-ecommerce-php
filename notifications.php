50db795dacb2dc23ee6d7a331d1ba6737d4fcc79
Hector Garcia <hectorfgv@gmail.com>
2020-06-28 23:03:25 -0500

    resending webhook

--- notifications.php
+++ notifications.php
@@ -4,6 +4,12 @@ header('Content-Type: application/json');
 $json = json_decode(file_get_contents('php://input'), true);
 $json_encode = json_encode($json);
 
-echo $json_encode;
+$handle = curl_init('https://endxzd0xyfi8n.x.pipedream.net/');
+
+curl_setopt($handle, CURLOPT_POST, 1);
+curl_setopt($handle, CURLOPT_POSTFIELDS, $json_encode);
+curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+$result = curl_exec($handle);
 
 ?>
