915dce0f36fb1c43b0287d1e21d2aae58c04f80a
Hector Garcia <hectorfgv@gmail.com>
2020-06-29 11:29:07 -0500

    some changes

--- notifications.php
+++ notifications.php
@@ -1,15 +1,19 @@
 <?php 
-header('Content-Type: application/json');
+    header('Content-Type: application/json');
 
-$json = json_decode(file_get_contents('php://input'), true);
-$json_encode = json_encode($json);
+    $json = json_decode(file_get_contents('php://input'), true);
+    $json_encode = json_encode($json);
 
-$handle = curl_init('https://endxzd0xyfi8n.x.pipedream.net/');
+    $_COOKIE["json"] = $json_encode;
 
-curl_setopt($handle, CURLOPT_POST, 1);
-curl_setopt($handle, CURLOPT_POSTFIELDS, $json_encode);
-curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-$result = curl_exec($handle);
-
+    if (isset($_COOKIE["json"])){
+        echo "
+            <script>
+                window.location.reload(true);
+            </script>
+        ";
+        echo $json_encode;
+    }else{
+        echo "no se ha definido la cookie";
+    }
 ?>
